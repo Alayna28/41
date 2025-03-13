e3cd0aba28e89cf90395b2d6cee7f2a43a907799
Alayna28 <alaynabaloch100@gmail.com>
2025-03-13 21:12:09 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#enter the temp
+temp = int(input("enter the temperature of your surrounding:"))
+if temp > 20:    
+elif:
+print("wear a jacket and soft clothe")
+  
+if temp < 30:
+ 
+elif:
+print("wear light clothes")
+
+
